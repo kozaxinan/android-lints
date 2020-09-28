fffde4fb2390cd690bc1d14e57852a327dda6b49
Jens <jens.schmitt.83@gmail.com>
2020-09-28 11:36:56 +0200

    WIP

---
+++ checks/src/main/java/com/kozaxinan/android/checks/NetworkLayerClassJsonDetector.kt
@@ -0,0 +1,74 @@
+package com.kozaxinan.android.checks
+
+import com.android.tools.lint.detector.api.Category.Companion.CORRECTNESS
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.Scope
+import com.android.tools.lint.detector.api.Severity.INFORMATIONAL
+import com.intellij.psi.PsiClass
+import org.jetbrains.uast.UAnnotated
+import org.jetbrains.uast.UField
+import org.jetbrains.uast.UMethod
+
+/**
+ * Check retrotif interface methods return type for SerializedName annotation.
+ */
+@Suppress("UnstableApiUsage")
+internal class NetworkLayerClassJsonDetector : RetrofitReturnTypeDetector() {
+
+  override fun createUastHandler(context: JavaContext) = NetworkLayerDtoFieldVisitor(context)
+
+  class NetworkLayerDtoFieldVisitor(private val context: JavaContext) : Visitor(context) {
+
+    override fun visitMethod(node: UMethod) {
+      val allFields = findAllFieldsOf(node)
+          .filterNot { !it.isStatic && it.containingClass?.isEnum == true }
+          .filterNot(::hasJsonNameAnnotation)
+      val nonFinalFields = allFields
+          .map { it.name }
+
+      if (nonFinalFields.isNotEmpty()) {
+        context.report(
+            issue = ISSUE_NETWORK_LAYER_CLASS_JSON_RULE,
+            scopeClass = node,
+            location = context.getNameLocation(node),
+            message = "Return type doesn't have @SerializedName annotation for $nonFinalFields fields."
+        )
+      }
+
+//      allFields.mapNotNull { it.containingClass }
+//          .toSet()
+//          .filterNot(::hasJsonClassAnnotation)
+    }
+
+    private fun hasJsonNameAnnotation(field: UField): Boolean {
+      return context
+          .evaluator
+          .getAllAnnotations(field as UAnnotated, true)
+          .mapNotNull { uAnnotation -> uAnnotation.qualifiedName }
+          .any { it.endsWith("Json") }
+    }
+
+    private fun hasJsonClassAnnotation(clazz: PsiClass): Boolean {
+      return context
+          .evaluator
+          .getAllAnnotations(clazz as UAnnotated, true)
+          .mapNotNull { uAnnotation -> uAnnotation.qualifiedName }
+          .any { it.endsWith("Json") }
+    }
+  }
+
+  companion object {
+
+    val ISSUE_NETWORK_LAYER_CLASS_JSON_RULE: Issue = Issue.create(
+        id = "NetworkLayerClassJsonRule",
+        briefDescription = "Json annotated network layer class",
+        explanation = "Data classes used in network layer should use Json annotation for Moshi. Adding annotation prevents obfuscation errors.",
+        category = CORRECTNESS,
+        priority = 5,
+        severity = INFORMATIONAL,
+        implementation = Implementation(NetworkLayerClassJsonDetector::class.java, Scope.JAVA_FILE_SCOPE)
+    )
+  }
+}
---
+++ checks/src/test/java/com/kozaxinan/android/checks/NetworkLayerClassJsonDetectorTest.kt
@@ -0,0 +1,630 @@
+package com.kozaxinan.android.checks
+
+import com.android.tools.lint.checks.infrastructure.TestFile
+import com.android.tools.lint.checks.infrastructure.TestFiles.bytes
+import com.android.tools.lint.checks.infrastructure.TestFiles.java
+import com.android.tools.lint.checks.infrastructure.TestFiles.kotlin
+import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import org.junit.Test
+
+private val ISSUE_TO_TEST = NetworkLayerClassJsonDetector.ISSUE_NETWORK_LAYER_CLASS_JSON_RULE
+
+@Suppress("UnstableApiUsage")
+internal class NetworkLayerClassJsonDetectorTest {
+
+  private fun retrofit(): TestFile.BinaryTestFile = bytes(
+      "libs/retrofit-2.7.2.jar",
+      javaClass
+          .getResourceAsStream("/retrofit-2.7.2.jar")
+          .readBytes()
+  )
+
+  private fun moshi(): TestFile.BinaryTestFile = bytes(
+      "libs/moshi-1.10.0.jar",
+      javaClass
+          .getResourceAsStream("/moshi-1.10.0.jar")
+          .readBytes()
+  )
+
+  @Test
+  fun `kotlin file with Json`() {
+    lint()
+        .files(
+            retrofit(),
+            moshi(),
+            kotlin(
+                """
+                package foo
+                
+                import retrofit2.http.GET
+                
+                interface Api {
+                
+                  @GET("url") 
+                  fun get(): Dto
+                }
+                """.trimIndent()
+            ),
+            kotlin(
+                """
+                package foo
+                
+                import import com.squareup.moshi.JsonClass
+                import import com.squareup.moshi.Json
+                
+                @JsonClass(generateAdapter = true)
+                data class Dto(
+                    @Json(name = "totalResults") val totalResults: Int,
+                    @Json(name = "totalNewResults") val totalNewResults: Int,
+                    @Json(name = "name") val name: String,
+                    @Json(name = "bool") val bool: Boolean
+                ) {
+                
+                  companion object {
+
+                    val EMPTY = Dto(0, 0, "", false)
+                  }
+                }
+                """.trimIndent()
+            )
+        )
+        .issues(ISSUE_TO_TEST)
+        .run()
+        .expectClean()
+  }
+
+//  @Test
+//  fun `kotlin enum file without SerializedName`() {
+//    lint()
+//        .files(
+//            retrofit(),
+//            moshi(),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import retrofit2.http.GET
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  fun get(): Dto
+//                }
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import com.google.gson.annotations.SerializedName
+//
+//                data class Dto(
+//                    @SerializedName("type") val type: PremiumType
+//                )
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import com.google.gson.annotations.SerializedName
+//
+//                enum class PremiumType {
+//
+//                  @SerializedName("SCHUFA") SCHUFA,
+//                  ARVATO;
+//                }
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expect(
+//            """
+//              src/foo/Api.kt:8: Information: Return type doesn't have @SerializedName annotation for [ARVATO] fields. [NetworkLayerClassSerializedNameRule]
+//                fun get(): Dto
+//                    ~~~
+//              0 errors, 0 warnings
+//            """.trimIndent()
+//        )
+//  }
+//
+//  @Test
+//  fun `kotlin enum with fields file without SerializedName`() {
+//    lint()
+//        .files(
+//            retrofit(),
+//            moshi(),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import retrofit2.http.GET
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  fun get(): Dto
+//                }
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import com.google.gson.annotations.SerializedName
+//
+//                data class Dto(
+//                    @SerializedName("type") val type: PremiumType
+//                )
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import com.google.gson.annotations.SerializedName
+//
+//                enum class PremiumType(val id: Int) {
+//
+//                  @SerializedName("SCHUFA") SCHUFA(1),
+//                  ARVATO(2);
+//                }
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expect(
+//            """
+//              src/foo/Api.kt:8: Information: Return type doesn't have @SerializedName annotation for [ARVATO] fields. [NetworkLayerClassSerializedNameRule]
+//                fun get(): Dto
+//                    ~~~
+//              0 errors, 0 warnings
+//            """.trimIndent()
+//        )
+//  }
+//
+//  @Test
+//  fun `kotlin file without SerializedName`() {
+//    lint()
+//        .files(
+//            retrofit(),
+//            moshi(),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import retrofit2.http.GET
+//
+//                internal interface Api {
+//
+//                  @GET("url")
+//                  fun get(): Dto
+//                }
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import com.google.gson.annotations.SerializedName
+//
+//                internal data class Dto(
+//                    @SerializedName("totalResults") val totalResults: Int,
+//                    val totalNewResults: Int,
+//                    @SerializedName("name") val name: String
+//                )
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expect(
+//            """
+//            src/foo/Api.kt:8: Information: Return type doesn't have @SerializedName annotation for [totalNewResults] fields. [NetworkLayerClassSerializedNameRule]
+//              fun get(): Dto
+//                  ~~~
+//            0 errors, 0 warnings
+//            """.trimIndent()
+//        )
+//  }
+//
+//  @Test
+//  fun `kotlin file without SerializedName for suspend method`() {
+//    lint()
+//        .files(
+//            retrofit(),
+//            moshi(),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import retrofit2.http.GET
+//
+//                internal interface Api {
+//
+//                  @GET("url")
+//                  suspend fun get(some:String, iasda: Int): Dto
+//                }
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import com.google.gson.annotations.SerializedName
+//
+//                internal data class Dto(
+//                    @SerializedName("totalResults") val totalResults: Int,
+//                    val totalNewResults: Int,
+//                    @SerializedName("name") val name: String
+//                )
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expect(
+//            """
+//              src/foo/Api.kt:8: Information: Return type doesn't have @SerializedName annotation for [totalNewResults] fields. [NetworkLayerClassSerializedNameRule]
+//                suspend fun get(some:String, iasda: Int): Dto
+//                            ~~~
+//              0 errors, 0 warnings
+//            """.trimIndent()
+//        )
+//  }
+//
+//  @Test
+//  fun `kotlin file without SerializedName from multiple interface`() {
+//    lint()
+//        .allowDuplicates()
+//        .files(
+//            retrofit(),
+//            moshi(),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import retrofit2.http.GET
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  fun get(): Dto
+//                }
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import retrofit2.http.GET
+//
+//                interface Api2 {
+//
+//                  @GET("url2")
+//                  fun get2(): Dto
+//                }
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                class Dto(
+//                    val totalResults: Int,
+//                    val totalNewResults: Int
+//                )
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expect(
+//            """
+//            src/foo/Api.kt:8: Information: Return type doesn't have @SerializedName annotation for [totalResults, totalNewResults] fields. [NetworkLayerClassSerializedNameRule]
+//              fun get(): Dto
+//                  ~~~
+//            src/foo/Api2.kt:8: Information: Return type doesn't have @SerializedName annotation for [totalResults, totalNewResults] fields. [NetworkLayerClassSerializedNameRule]
+//              fun get2(): Dto
+//                  ~~~~
+//            0 errors, 0 warnings
+//            """.trimIndent()
+//        )
+//  }
+//
+//  @Test
+//  fun `kotlin file inner dto without SerializedName`() {
+//    lint()
+//        .files(
+//            retrofit(),
+//            moshi(),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import retrofit2.http.GET
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  fun get(): Dto
+//                }
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                class Dto(
+//                    val totalResults: Int,
+//                    var innerDto: InnerDto
+//                )
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                class InnerDto(
+//                    var innerResults: Int
+//                )
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expect(
+//            """
+//            src/foo/Api.kt:8: Information: Return type doesn't have @SerializedName annotation for [totalResults, innerDto, innerResults] fields. [NetworkLayerClassSerializedNameRule]
+//              fun get(): Dto
+//                  ~~~
+//            0 errors, 0 warnings
+//            """.trimIndent()
+//        )
+//  }
+//
+//  @Test
+//  fun `kotlin file return type generic without SerializedName`() {
+//    lint()
+//        .files(
+//            retrofit(),
+//            moshi(),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import retrofit2.Call
+//                import retrofit2.http.GET
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  fun get(): Call<List<Dto>>
+//                }
+//                """.trimIndent()
+//            ),
+//            kotlin(
+//                """
+//                package foo
+//
+//                class Dto(
+//                    val totalResults: Int,
+//                    val totalNewResults: Int
+//                )
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expect(
+//            """
+//            src/foo/Api.kt:9: Information: Return type doesn't have @SerializedName annotation for [totalResults, totalNewResults] fields. [NetworkLayerClassSerializedNameRule]
+//              fun get(): Call<List<Dto>>
+//                  ~~~
+//            0 errors, 0 warnings
+//            """.trimIndent()
+//        )
+//  }
+//
+//  @Test
+//  fun `kotlin file return type generic with Unit`() {
+//    lint()
+//        .files(
+//            retrofit(),
+//            moshi(),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import retrofit2.Call
+//                import retrofit2.http.GET
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  fun get(): Call<List<Unit>>
+//                }
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expectClean()
+//  }
+//
+//  @Test
+//  fun `kotlin file with Unit`() {
+//    lint()
+//        .files(
+//            retrofit(),
+//            moshi(),
+//            kotlin(
+//                """
+//                package foo
+//
+//                import retrofit2.http.GET
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  fun get(): Unit
+//                }
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expectClean()
+//  }
+//
+//  @Test
+//  fun `java file with SerializedName`() {
+//    lint()
+//        .files(
+//            java(
+//                """
+//                package foo;
+//
+//                import retrofit2.http.GET;
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  Dto get();
+//                }
+//                """.trimIndent()
+//            ),
+//            java(
+//                """
+//                package foo;
+//
+//                import com.google.gson.annotations.SerializedName;
+//
+//                class Dto {
+//                    @SerializedName("a") final int totalResults;
+//                    @SerializedName("b") final int totalNewResults;
+//
+//                    Dto(int totalResults, int totalNewResults) {
+//                      this.totalResult = totalResults;
+//                      this.totalNewResults = totalNewResults;
+//                    }
+//                }
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expectClean()
+//  }
+//
+//  @Test
+//  fun `java file with Void`() {
+//    lint()
+//        .files(
+//            java(
+//                """
+//                package foo;
+//
+//                import retrofit2.http.GET;
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  Void get();
+//                }
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expectClean()
+//  }
+//
+//  @Test
+//  fun `java file generic method with Void`() {
+//    lint()
+//        .files(
+//            java(
+//                """
+//                package foo;
+//
+//                import retrofit2.Call;
+//                import retrofit2.http.GET;
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  Call<Void> get();
+//
+//                  @GET("url")
+//                  Call<Void> get2();
+//                }
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expectClean()
+//  }
+//
+//  @Test
+//  fun `java file without SerializedName`() {
+//    lint()
+//        .files(
+//            java(
+//                """
+//                package foo;
+//
+//                import retrofit2.http.GET;
+//
+//                interface Api {
+//
+//                  @GET("url")
+//                  Dto get();
+//
+//                  @GET("url")
+//                  Dto get2();
+//                }
+//                """.trimIndent()
+//            ),
+//            java(
+//                """
+//                package foo;
+//
+//                import com.google.gson.annotations.SerializedName;
+//
+//                import java.util.ArrayList;
+//                import java.util.List;
+//
+//                class Dto {
+//
+//                    @SerializedName("a") final int totalResults;
+//                    int totalNewResults;
+//
+//                    private static final List<Dto> list = new ArrayList<>();
+//
+//                    Dto(int totalResults, int totalNewResults) {
+//                      this.totalResult = totalResults;
+//                      this.totalNewResults = totalNewResults;
+//                    }
+//                }
+//                """.trimIndent()
+//            )
+//        )
+//        .issues(ISSUE_TO_TEST)
+//        .run()
+//        .expect(
+//            """
+//            src/foo/Api.java:8: Information: Return type doesn't have @SerializedName annotation for [totalNewResults] fields. [NetworkLayerClassSerializedNameRule]
+//              Dto get();
+//                  ~~~
+//            src/foo/Api.java:11: Information: Return type doesn't have @SerializedName annotation for [totalNewResults] fields. [NetworkLayerClassSerializedNameRule]
+//              Dto get2();
+//                  ~~~~
+//            0 errors, 0 warnings
+//            """.trimIndent()
+//        )
+//  }
+}
